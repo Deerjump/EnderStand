f34d9bed4843b6a874240de7f1b93b4720781c53
jbillman <billman377@gmail.com>
2025-02-22 19:54:56 -0500

    Project Setup

---
+++ src/main/java/io/github/deerjump/enderstand/EnderStandMod.java
@@ -0,0 +1,17 @@
+package io.github.deerjump.enderstand;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(EnderStandMod.MOD_ID)
+public class EnderStandMod
+{
+    public static final String MOD_ID = "enderstand";
+
+    public EnderStandMod(IEventBus modEventBus, ModContainer modContainer)
+    {
+
+    }
+
+}
